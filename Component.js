88696fdc29308636fa8285c7b451168ef73fe4fc
YauhenKurlovich <yauhen.kurlovich@leverx.com>
2019-01-25 14:03:54 +0300

    task 10 (lecture practice2 is done)

--- Component.js
+++ Component.js
@@ -3,19 +3,17 @@ sap.ui.define([
 ], function (UIComponent) {
 	"use strict";
 
-	return UIComponent.extend("sap.ui.demo.nav.Component", {
+	return UIComponent.extend("sap.ui.demo.db.Component", {
 
 		metadata: {
 			manifest: "json"
 		},
 
-		init: function () {
-			// call the init function of the parent
-			UIComponent.prototype.init.apply(this, arguments);
-
-			// create the views based on the url/hash
-			this.getRouter().initialize();
-		}
+        init: function () {
+            UIComponent.prototype.init.apply(this, arguments);
+            this.getRouter().initialize();
+            
+        }
 
 	});
 
--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,36 +1,27 @@
 sap.ui.define([
-	"sap/ui/demo/nav/controller/BaseController",
-	"sap/base/Log"
-], function(BaseController, Log) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/UIComponent"
+], function (Controller, UIComponent) {
 	"use strict";
 
-	return BaseController.extend("sap.ui.demo.nav.controller.App", {
-
-		onInit: function () {
-			// This is ONLY for being used within the tutorial.
-			// The default log level of the current running environment may be higher than INFO,
-			// in order to see the debug info in the console, the log level needs to be explicitly
-			// set to INFO here.
-			// But for application development, the log level doesn't need to be set again in the code.
-			Log.setLevel(Log.Level.INFO);
-
-			var oRouter = this.getRouter();
-
-			oRouter.attachBypassed(function (oEvent) {
-				var sHash = oEvent.getParameter("hash");
-				// do something here, i.e. send logging data to the backend for analysis
-				// telling what resource the user tried to access...
-				Log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
-			});
-
-			oRouter.attachRouteMatched(function (oEvent){
-				var sRouteName = oEvent.getParameter("name");
-				// do something, i.e. send usage statistics to backend
-				// in order to improve our app and the user experience (Build-Measure-Learn cycle)
-				Log.info("User accessed route " + sRouteName + ", timestamp = " + new Date().getTime());
-			});
+	return Controller.extend("sap.ui.demo.db.controller.App", {
+		onInit: function(){
+			console.log("oninit");
+		},
+		getRouter : function () {
+			return UIComponent.getRouterFor(this);
+		},
+		productListFactory : function(sId, oContext) {
+			var oUIControl;
+			oUIControl = this.byId("productExtended").clone(sId);
+			return oUIControl;
+		},		
+		onItemSelected: function(oEvent) {
+			var oSelectedItem = oEvent.getSource();
+			var context = encodeURIComponent(oSelectedItem.getBindingContext('people').getPath());
+			//console.log(oSelectedItem.getBindingContext('people').getPath());
+			console.log(context);
+			this.getRouter().navTo("detail",  {invoicePath: context});
 		}
-
-	});
-
+    });
 });
